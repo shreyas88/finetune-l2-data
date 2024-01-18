5ceea4e1697d77c679953dea72a69a48a736ecae
Shreyas Srivastava <shreyas.srivastava@gmail.com>
2024-01-18 12:59:43 +0530

    add auto train config

---
+++ autotrain_config.py
@@ -0,0 +1,40 @@
+import os
+
+learning_rate = 2e-4
+num_epochs = 4
+batch_size = 1
+block_size = 1024
+trainer = "sft"
+warmup_ratio = 0.1
+weight_decay = 0.01
+gradient_accumulation = 4
+use_fp16 = True
+use_peft = True
+use_int4 = True
+lora_r = 16
+lora_alpha = 32
+lora_dropout = 0.045
+
+project_name = 'myllm'
+model_name = 'mistralai/Mistral-7B-Instruct-v0.1'
+push_to_hub = False
+hf_token = "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"
+repo_id = "username/repo_name
+os.environ["PROJECT_NAME"] = project_name
+os.environ["MODEL_NAME"] = model_name
+os.environ["PUSH_TO_HUB"] = str(push_to_hub)
+os.environ["HF_TOKEN"] = hf_token
+os.environ["REPO_ID"] = repo_id
+os.environ["LEARNING_RATE"] = str(learning_rate)
+os.environ["NUM_EPOCHS"] = str(num_epochs)
+os.environ["BATCH_SIZE"] = str(batch_size)
+os.environ["BLOCK_SIZE"] = str(block_size)
+os.environ["WARMUP_RATIO"] = str(warmup_ratio)
+os.environ["WEIGHT_DECAY"] = str(weight_decay)
+os.environ["GRADIENT_ACCUMULATION"] = str(gradient_accumulation)
+os.environ["USE_FP16"] = str(use_fp16)
+os.environ["USE_PEFT"] = str(use_peft)
+os.environ["USE_INT4"] = str(use_int4)
+os.environ["LORA_R"] = str(lora_r)
+os.environ["LORA_ALPHA"] = str(lora_alpha)
+os.environ["LORA_DROPOUT"] = str(lora_dropout)
