b258630ff7a5f444b139a920eb8af40a2b19d48b
Shreyas Srivastava <shreyas.srivastava@gmail.com>
2024-02-03 18:56:43 +0530

    Fix type error

--- embeddings_finetune.py
+++ embeddings_finetune.py
@@ -2,15 +2,19 @@ from typing import Any, List
 
 from llama_index.bridge.pydantic import PrivateAttr
 from llama_index.embeddings.base import BaseEmbedding
-from transformers import AutoModelForCausalLM, AutoTokenizer
+from transformers import AutoModelForCausalLM, AutoTokenizer, PreTrainedTokenizer, PreTrainedModel
 import torch
 
 
 class WOBEmbeddings(BaseEmbedding):
+    _model_path: str  = PrivateAttr()
+    _tokenizer: PreTrainedTokenizer = PrivateAttr()
+    _model: PreTrainedModel = PrivateAttr()
+
     def __init__(self, **kwargs: Any) -> None:
-      self.model_path = "myllm-finetune_fp16_batch16"
-      self.tokenizer = AutoTokenizer.from_pretrained(model_path)
-      self.model = AutoModelForCausalLM.from_pretrained(model_path)
+      self._model_path = "myllm-finetune_fp16_batch16"
+      self._tokenizer = AutoTokenizer.from_pretrained(model_path)
+      self._model = AutoModelForCausalLM.from_pretrained(model_path)
       super().__init__(**kwargs)
 
     @classmethod
@@ -24,8 +28,8 @@ class WOBEmbeddings(BaseEmbedding):
       return self._get_text_embedding(text)
 
     def embed_text(self, text: str) -> List[float]:
-      input_ids = tokenizer.encode(text, return_tensors="pt")
-      last_hidden_state = self.model(**{"input_ids":input_ids, "output_hidden_states":True}).hidden_states[-1]
+      input_ids = self._tokenizer.encode(text, return_tensors="pt")
+      last_hidden_state = self._model(**{"input_ids":input_ids, "output_hidden_states":True}).hidden_states[-1]
       return torch.sum(last_hidden_state, dim=1)
 
     def _get_query_embedding(self, query: str) -> List[float]:
