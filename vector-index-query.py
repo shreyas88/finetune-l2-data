5dcbd5dc9d104f4dc242a799885cf01e3dd10d4e
Shreyas Srivastava <shreyas.srivastava@gmail.com>
2024-02-04 12:14:22 +0530

    add hugging face embedding support

--- vector-index-query.py
+++ vector-index-query.py
@@ -1,7 +1,7 @@
-from llama_index import Document,VectorStoreIndex,ServiceContext
+from llama_index import Document,VectorStoreIndex,ServiceContext,LangchainEmbedding
+from langchain.embeddings.huggingface import HuggingFaceEmbeddings
 import pandas as pd
 import argparse
-from embeddings_finetune import WOBEmbeddings
 
 # Initialize parser
 parser = argparse.ArgumentParser(description="Generate text from a language model")
@@ -14,10 +14,10 @@ df = pd.read_csv('data/train_chat.csv')
 df['finalText'] = df['instruction']+" " + df['text']
 docs = [Document(text=t[1]) for t in df['finalText'].items()]
 
+embed_model = LangchainEmbedding(HuggingFaceEmbeddings(model_name="myllm-finetune_fp16_batch16"))
+
 # embedding generation
-service_context = ServiceContext.from_defaults(
-    embed_model=WOBEmbeddings()
-)
+service_context = ServiceContext.from_defaults(llm=None,embed_model=embed_model)
 index = VectorStoreIndex.from_documents(docs, service_context=service_context, show_progress=True)
 
 query_engine = index.as_query_engine()
