718ede915fbb56d563a12d4a07dffe493e0202ad
Shreyas Srivastava <shreyas.srivastava@gmail.com>
2024-02-05 12:59:06 +0530

    add support for batch inference

--- embeddings_finetune.py
+++ embeddings_finetune.py
@@ -20,6 +20,14 @@ class WOBEmbeddings(BaseEmbedding):
     @classmethod
     def class_name(cls) -> str:
       return "wobembeddings"
+    
+    def encode(self, texts):
+      encoded_dict = self._tokenizer.batch_encode_plus(texts, add_special_tokens=True, max_length=160, 
+                                                       padding='max_length',
+                                                       return_attention_mask=True, truncation=True, return_tensors='pt')
+      input_ids = encoded_dict['input_ids']
+      attention_masks = encoded_dict['attention_mask']
+      return input_ids, attention_masks
 
     async def _aget_query_embedding(self, query: str) -> List[float]:
       return self._get_query_embedding(query)
@@ -33,8 +41,9 @@ class WOBEmbeddings(BaseEmbedding):
       return torch.sum(last_hidden_state, dim=1).squeeze(0).tolist()
     
     def batch_embed_text(self, texts: List[str]) -> List[List[float]]:
-      input_ids = torch.stack([ self._tokenizer.encode(text, return_tensors="pt") for text in texts ])
-      last_hidden_state = self._model(**{"input_ids":input_ids, "output_hidden_states":True}).hidden_states[-1]
+      input_ids, attention_masks = self.encode(texts)
+      last_hidden_state = self._model(**{"input_ids":input_ids, "attention_mask":attention_masks, 
+                                         "output_hidden_states":True}).hidden_states[-1]
       list_tensors = torch.sum(last_hidden_state, dim=1).tolist()
       return [ tensor.squeeze(0).tolist() for tensor in list_tensors ]
 
