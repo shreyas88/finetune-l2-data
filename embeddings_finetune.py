3b3aeff11e9770bb5c5728e5a8a6cf6a214e6450
Shreyas Srivastava <shreyas.srivastava@gmail.com>
2024-02-05 19:48:27 +0530

    gpu

--- embeddings_finetune.py
+++ embeddings_finetune.py
@@ -15,6 +15,7 @@ class WOBEmbeddings(BaseEmbedding):
       self._model_path = "myllm-finetune_fp16_batch16"
       self._tokenizer = AutoTokenizer.from_pretrained(self._model_path)
       self._model = AutoModelForCausalLM.from_pretrained(self._model_path)
+      self._model.cuda()
       super().__init__(**kwargs)
 
     @classmethod
@@ -37,16 +38,15 @@ class WOBEmbeddings(BaseEmbedding):
 
     def embed_text(self, text: str) -> List[float]:
       #self._model.cuda()
-      input_ids = self._tokenizer.encode(text, return_tensors="pt")
-      last_hidden_state = self._model(**{"input_ids":input_ids, "output_hidden_states":True}).hidden_states[-1]
+      input_ids = self._tokenizer.encode(text, return_tensors="pt").cuda()
+      last_hidden_state = self._model(**{"input_ids":input_ids, "output_hidden_states":True}).hidden_states[-1].detach().cpu()   
       return torch.sum(last_hidden_state, dim=1).squeeze(0).tolist()
     
     def batch_embed_text(self, texts: List[str]) -> List[List[float]]:
-      self._model.cuda()
       input_ids, attention_masks = self.encode(texts)
       last_hidden_state = self._model(**{"input_ids":input_ids, "attention_mask":attention_masks, 
                                          "output_hidden_states":True}).hidden_states[-1].detach().cpu()      
-      self._model.cpu()
+      #self._model.cpu()
       return torch.sum(last_hidden_state, dim=1).tolist()
 
     def _get_query_embedding(self, query: str) -> List[float]:
